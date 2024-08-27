688c500dd067157158bea4f549e704ba90949af4
Cliffdoyle <oyoocliff471@gmail.com>
2024-08-27 11:45:32 +0300

    Add:Created program that checks the position Santa first enters the basement

---
+++ Day1Part_2/day1_part2_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestCount(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"upstairs",args{"()())"},5},
+		{"downstairs",args{")"},1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Count(tt.args.s); got != tt.want {
+				t.Errorf("Count() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
